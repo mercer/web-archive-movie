559ab4bfa9c1e43648e6486f47a3673cd0b8ffad
Mercer Traieste <mercer.traieste@gmail.com>
2015-07-20 05:42:07 +0000

    added call to wayback api with node request module

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var path = require('path')
-var childProcess = require('child_process')
-var phantomjs = require('phantomjs')
-var binPath = phantomjs.path
- 
-var childArgs = [
-  path.join(__dirname, 'grab-page.js'),
-  'http://www.google.com'
-  
-]
- 
-childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
-  // handle results 
-})
+var path = require('path');
+var childProcess = require('child_process');
+var phantomjs = require('phantomjs');
+var binPath = phantomjs.path;
+var request = require('request');
+
+request
+  .get('https://archive.org/wayback/available?url=google.com&timestamp=20150101',
+  function (error, response, body) {
+    var page = JSON.parse(body).archived_snapshots.closest.url;
+    var childArgs = [path.join(__dirname, 'grab-page.js'), page];
+    childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {})
+  });
